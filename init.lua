8d5ea950f7e8ab8554c709f6a9fddae1371b1f50
thybux <faivre.thomas88@free.fr>
2025-03-04 17:32:45 +0100

    update

--- init.lua
+++ init.lua
@@ -8,308 +8,267 @@ vim.opt.relativenumber = true
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 -- Installer lazy.nvim s'il n'est pas déjà installé
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- chrager l'ui
-require("ui_colors")
+-- require("ui_colors")
+
+vim.filetype.add({
+  extension = {
+    tpl = "html",
+  },
+})
 
 -- Configuration des plugins avec lazy.nvim
 require("lazy").setup({
-	-- Mason pour gérer les LSP
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"rcarriga/nvim-notify",
-		config = function()
-			vim.notify = require("notify")
-		end,
-	},
-
-	-- ajout du formater de code au moment du :w
-	{
-		"mhartington/formatter.nvim",
-		config = function()
-			require("formatter").setup({
-				filetype = {
-					lua = {
-						function()
-							return {
-								exe = "stylua",
-								args = { "--search-parent-directories", "-" },
-								stdin = true,
-							}
-						end,
-					},
-					python = {
-						function()
-							return {
-								exe = "black",
-								args = { "-" },
-								stdin = true,
-							}
-						end,
-					},
-					javascript = {
-						function()
-							return {
-								exe = "prettier",
-								args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
-								stdin = true,
-							}
-						end,
-					},
-					typescript = {
-						function()
-							return {
-								exe = "prettier",
-								args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
-								stdin = true,
-							}
-						end,
-					},
-					rust = {
-						function()
-							return {
-								exe = "rustfmt",
-								args = { "--emit=stdout" },
-								stdin = true,
-							}
-						end,
-					},
-				},
-			})
-
-			-- Automatically format on save
-			vim.api.nvim_exec(
-				[[
-          augroup FormatAutogroup
-            autocmd!
-            autocmd BufWritePost * FormatWrite
-          augroup END
-        ]],
-				true
-			)
-		end,
-	},
-
-	-- mason-lspconfig pour une intégration facile avec lspconfig
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"neovim/nvim-lspconfig",
-		},
-		config = function()
-			require("mason-lspconfig").setup()
-			require("mason-lspconfig").setup_handlers({
-				function(server_name)
-					require("lspconfig")[server_name].setup({
-						capabilities = require("cmp_nvim_lsp").default_capabilities(),
-					})
-				end,
-			})
-		end,
-	},
-	--[[{
-    'Mofiqul/dracula.nvim',
+  -- Mason pour gérer les LSP
+  {
+    "williamboman/mason.nvim",
     config = function()
-      require('dracula').setup({
+      require("mason").setup()
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+  },
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      vim.notify = require("notify")
+    end,
+  },
+  -- mason-lspconfig pour une intégration facile avec lspconfig
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason-lspconfig").setup()
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          require("lspconfig")[server_name].setup({
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+          })
+        end,
+      })
+    end,
+  },
+  {
+    "Mofiqul/dracula.nvim",
+    config = function()
+      require("dracula").setup({
         -- Activer les options
-        transparent_bg = false, -- Désactiver la transparence (fond opaque)
-        italic_comment = true,  -- Commentaires en italique (facultatif)
+        transparent_bg = false,
+        italic_comment = true,
       })
       vim.cmd("colorscheme dracula") -- Applique le thème
     end,
-  },--]]
+  },
+  --
 
-	-- Configuration des LSP
-	{
-		"neovim/nvim-lspconfig",
-	},
-	-- Affichage des indentations (version 2)
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		version = "v2", -- Spécifier la version 2
-		config = function()
-			require("indent_blankline").setup({
-				show_current_context = true,
-				show_current_context_start = true,
-			})
-		end,
-	},
-	-- Affichage des couleurs hexadécimales
-	{
-		"norcalli/nvim-colorizer.lua",
-		config = function()
-			require("colorizer").setup()
-		end,
-	},
-	-- Navigation améliorée avec Harpoon
-	{
-		"ThePrimeagen/harpoon",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("harpoon").setup()
-			-- Raccourcis pour Harpoon
-			vim.keymap.set("n", "<leader>a", require("harpoon.mark").add_file)
-			vim.keymap.set("n", "<leader>h", require("harpoon.ui").toggle_quick_menu)
-		end,
-	},
-	-- Coloration syntaxique avancée avec Treesitter
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate", -- Remplace 'run' par 'build' pour lazy.nvim
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "c", "lua", "python", "javascript" }, -- Ajoutez les langages que vous utilisez
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-				indent = {
-					enable = true,
-				},
-			})
-		end,
-	},
-	-- Recherche de fichiers avec Telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({})
-			-- Raccourcis pour Telescope
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-		end,
-	},
-	-- Autocomplétion avec nvim-cmp
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp", -- Source pour LSP
-			"hrsh7th/cmp-buffer", -- Source pour buffer actuel
-			"hrsh7th/cmp-path", -- Source pour les chemins de fichiers
-			"hrsh7th/cmp-cmdline", -- Source pour la ligne de commande
-			"L3MON4D3/LuaSnip", -- Moteur de snippets
-			"saadparwaiz1/cmp_luasnip", -- Source pour LuaSnip
-			"rafamadriz/friendly-snippets", -- Snippets prédéfinis
-		},
-		config = function()
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
+  -- Configuration des LSP
+  {
+    "neovim/nvim-lspconfig",
+  },
+  -- Affichage des indentations (version 2)
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    version = "v2", -- Spécifier la version 2
+    config = function()
+      require("indent_blankline").setup({
+        show_current_context = true,
+        show_current_context_start = true,
+      })
+    end,
+  },
+  -- Affichage des couleurs hexadécimales
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
+  },
+  {
+    'stevearc/conform.nvim',
+    opts = {},
+  },
+  -- Navigation améliorée avec Harpoon
+  {
+    "ThePrimeagen/harpoon",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("harpoon").setup()
+      -- Raccourcis pour Harpoon
+      vim.keymap.set("n", "<leader>a", require("harpoon.mark").add_file)
+      vim.keymap.set("n", "<leader>h", require("harpoon.ui").toggle_quick_menu)
+    end,
+  },
+  -- Coloration syntaxique avancée avec Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate", -- Remplace 'run' par 'build' pour lazy.nvim
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "lua", "python", "javascript" }, -- Ajoutez les langages que vous utilisez
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
+  -- Recherche de fichiers avec Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({})
+      -- Raccourcis pour Telescope
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    end,
+  },
+  -- Autocomplétion avec nvim-cmp
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",      -- Source pour LSP
+      "hrsh7th/cmp-buffer",        -- Source pour buffer actuel
+      "hrsh7th/cmp-path",          -- Source pour les chemins de fichiers
+      "hrsh7th/cmp-cmdline",       -- Source pour la ligne de commande
+      "L3MON4D3/LuaSnip",          -- Moteur de snippets
+      "saadparwaiz1/cmp_luasnip",  -- Source pour LuaSnip
+      "rafamadriz/friendly-snippets", -- Snippets prédéfinis
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
 
-			-- Charger les snippets prédéfinis
-			require("luasnip.loaders.from_vscode").lazy_load()
+      -- Charger les snippets prédéfinis
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-					{ name = "path" },
-				}),
-			})
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
 
-			-- Configuration pour la ligne de commande
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "path" },
-					{ name = "cmdline" },
-				},
-			})
-		end,
-	},
-	-- Gestion des paires avec nvim-autopairs
-	{
-		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup({})
-			-- Intégration avec nvim-cmp
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			local cmp = require("cmp")
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-		end,
-	},
-	-- Arborescence des fichiers avec nvim-tree
-	{
-		"nvim-tree/nvim-tree.lua",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		config = function()
-			require("nvim-tree").setup({})
-		end,
-	},
-	-- Barre des fichiers ouverts avec bufferline
-	{
-		"akinsho/bufferline.nvim",
-		version = "*",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		config = function()
-			require("bufferline").setup({})
-		end,
-	},
-	-- Terminal intégré avec toggleterm
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		config = function()
-			require("toggleterm").setup({})
-		end,
-	},
-	-- Jeu pour s'entraîner aux commandes Vim
-	{
-		"ThePrimeagen/vim-be-good",
-		cmd = { "VimBeGood" },
-	},
+      -- Configuration pour la ligne de commande
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "path" },
+          { name = "cmdline" },
+        },
+      })
+    end,
+  },
+  -- Gestion des paires avec nvim-autopairs
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup({})
+      -- Intégration avec nvim-cmp
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+  },
+  -- Arborescence des fichiers avec nvim-tree
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("nvim-tree").setup({})
+    end,
+  },
+  -- Barre des fichiers ouverts avec bufferline
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("bufferline").setup({})
+    end,
+  },
+  -- Terminal intégré avec toggleterm
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({})
+    end,
+  },
+  -- Jeu pour s'entraîner aux commandes Vim
+  {
+    "ThePrimeagen/vim-be-good",
+    cmd = { "VimBeGood" },
+  },
+})
+
+
+require("conform").setup({
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
 })
 
 -- Ouvrir nvim-tree avec <leader>e
@@ -321,18 +280,14 @@ vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>", { noremap = true, silent = t
 -- Lancer le jeu vim-be-good avec <leader>g
 vim.keymap.set("n", "<leader>g", ":VimBeGood<CR>", { noremap = true, silent = true })
 
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "python",
-	callback = function()
-		local python_path = get_python_path()
-		require("lspconfig").pyright.setup({
-			settings = {
-				python = {
-					pythonPath = python_path,
-				},
-			},
-		})
-	end,
+-- Configuration de LSP
+local lspconfig = require("lspconfig")
+
+-- Configuration HTML LSP pour les templates
+
+-- Configuration Emmet pour l'écriture rapide
+lspconfig.emmet_ls.setup({
+  filetypes = { "html", "css", "tpl" },
 })
 
 vim.opt.guifont = "JetBrains Mono:h14"
--- lua/ui_colors.lua
+++ lua/ui_colors.lua
@@ -1,86 +1,127 @@
--- JetBrains New UI Inspired Colorscheme for Neovim
--- Save this file as a Lua module and source it in your init.lua
--- Example: require('jetbrains_ui_colors')
+-- Dracula No Background Colorscheme for Neovim
+-- Save this file as ~/.config/nvim/lua/dracula_no_bg.lua
 
-local custom_colorscheme = function()
-	-- Background and foreground
-	-- vim.cmd('highlight Normal guibg=#2b2d30 guifg=#dcdcdc')
+local dracula_no_bg = function()
+	-- Palette Dracula originale
+	local fg = "#f8f8f2" -- Texte principal
+	local comment = "#6272a4" -- Commentaires
+	local selection = "#44475a" -- Sélection
+	local cyan = "#8be9fd" -- Cyan
+	local green = "#50fa7b" -- Vert
+	local orange = "#ffb86c" -- Orange
+	local pink = "#ff79c6" -- Rose
+	local purple = "#bd93f9" -- Violet
+	local red = "#ff5555" -- Rouge
+	local yellow = "#f1fa8c" -- Jaune
 
-	-- Comments
-	vim.cmd("highlight Comment guifg=#808080 gui=italic")
+	-- Reset toutes les couleurs de fond pour qu'elles soient transparentes
+	vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+	vim.cmd("highlight SignColumn guibg=NONE ctermbg=NONE")
+	vim.cmd("highlight LineNr guibg=NONE ctermbg=NONE")
+	vim.cmd("highlight CursorLineNr guibg=NONE ctermbg=NONE")
+	vim.cmd("highlight VertSplit guibg=NONE ctermbg=NONE")
+	vim.cmd("highlight StatusLine guibg=NONE ctermbg=NONE")
+	vim.cmd("highlight StatusLineNC guibg=NONE ctermbg=NONE")
+	vim.cmd("highlight NonText guibg=NONE ctermbg=NONE")
 
-	-- Brackets
-	vim.cmd("highlight Bracket guifg=#dcdcdc")
+	-- Éléments de base
+	vim.cmd("highlight Normal guifg=" .. fg)
+	vim.cmd("highlight Comment guifg=" .. comment .. " gui=italic")
 
-	-- Variable types
-	vim.cmd("highlight Type guifg=#ffcb6b")
+	-- Curseur et ligne active
+	vim.cmd("highlight Cursor guifg=NONE guibg=" .. fg)
+	vim.cmd("highlight CursorLine guibg=" .. selection .. " ctermbg=NONE")
+	vim.cmd("highlight CursorLineNr guifg=" .. yellow .. " gui=bold")
 
-	-- Add highlights for curly brackets
-	vim.cmd("highlight Delimiter guifg=#c8c8c8")
+	-- Sélection et recherche
+	vim.cmd("highlight Visual guibg=" .. selection)
+	vim.cmd("highlight Search guibg=" .. orange .. " guifg=#000000")
+	vim.cmd("highlight IncSearch guibg=" .. green .. " guifg=#000000")
 
-	-- Keywords
-	vim.cmd("highlight Keyword guifg=#c792ea")
+	-- Mots-clés et contrôle de flux
+	vim.cmd("highlight Keyword guifg=" .. pink)
+	vim.cmd("highlight Statement guifg=" .. pink)
+	vim.cmd("highlight Conditional guifg=" .. pink)
+	vim.cmd("highlight Repeat guifg=" .. pink)
+	vim.cmd("highlight Label guifg=" .. pink)
+	vim.cmd("highlight Exception guifg=" .. pink)
 
-	-- Functions
-	vim.cmd("highlight Function guifg=#82aaff")
+	-- Types et structures
+	vim.cmd("highlight Type guifg=" .. cyan .. " gui=italic")
+	vim.cmd("highlight StorageClass guifg=" .. pink)
+	vim.cmd("highlight Structure guifg=" .. cyan)
+	vim.cmd("highlight Typedef guifg=" .. cyan)
 
-	-- Strings
-	vim.cmd("highlight String guifg=#c3e88d")
+	-- Fonctions et variables
+	vim.cmd("highlight Function guifg=" .. green .. " gui=bold")
+	vim.cmd("highlight Identifier guifg=" .. cyan .. " gui=italic")
 
-	-- Variables
-	vim.cmd("highlight Identifier guifg=#f78c6c")
+	-- Constantes et littéraux
+	vim.cmd("highlight Constant guifg=" .. purple)
+	vim.cmd("highlight String guifg=" .. yellow)
+	vim.cmd("highlight Character guifg=" .. yellow)
+	vim.cmd("highlight Number guifg=" .. purple)
+	vim.cmd("highlight Boolean guifg=" .. purple)
+	vim.cmd("highlight Float guifg=" .. purple)
 
-	-- Numbers
-	vim.cmd("highlight Number guifg=#ff5370")
+	-- Préprocesseur
+	vim.cmd("highlight PreProc guifg=" .. pink)
+	vim.cmd("highlight Include guifg=" .. pink)
+	vim.cmd("highlight Define guifg=" .. pink)
+	vim.cmd("highlight Macro guifg=" .. pink)
+	vim.cmd("highlight PreCondit guifg=" .. pink)
 
-	-- Visual mode selection
-	vim.cmd("highlight Visual guibg=#3e4451")
+	-- Opérateurs et spéciaux
+	vim.cmd("highlight Operator guifg=" .. pink)
+	vim.cmd("highlight Special guifg=" .. pink)
+	vim.cmd("highlight SpecialChar guifg=" .. pink)
+	vim.cmd("highlight Tag guifg=" .. pink)
+	vim.cmd("highlight Delimiter guifg=" .. fg)
+	vim.cmd("highlight SpecialComment guifg=" .. comment .. " gui=bold")
+	vim.cmd("highlight Debug guifg=" .. pink)
 
-	-- Error messages
-	vim.cmd("highlight Error guifg=#ff5370")
+	-- PHP spécifique
+	vim.cmd("highlight phpVarSelector guifg=" .. pink)
+	vim.cmd("highlight phpVariable guifg=" .. orange)
+	vim.cmd("highlight phpIdentifier guifg=" .. orange)
+	vim.cmd("highlight phpType guifg=" .. cyan)
+	vim.cmd("highlight phpFunction guifg=" .. green .. " gui=bold")
+	vim.cmd("highlight phpMethod guifg=" .. green .. " gui=bold")
+	vim.cmd("highlight phpClass guifg=" .. cyan .. " gui=italic")
+	vim.cmd("highlight phpDocTags guifg=" .. comment .. " gui=bold")
+	vim.cmd("highlight phpDocParam guifg=" .. comment)
+	vim.cmd("highlight phpMemberSelector guifg=" .. fg)
+	vim.cmd("highlight phpOperator guifg=" .. pink)
+	vim.cmd("highlight phpStaticClasses guifg=" .. cyan .. " gui=italic")
+	vim.cmd("highlight phpStringDouble guifg=" .. yellow)
+	vim.cmd("highlight phpStringSingle guifg=" .. yellow)
+	vim.cmd("highlight phpKeyword guifg=" .. pink)
+	vim.cmd("highlight phpSqlKeyword guifg=" .. pink)
+	vim.cmd("highlight phpSqlString guifg=" .. yellow)
 
-	-- Line numbers
-	vim.cmd("highlight LineNr guifg=#4b5263")
+	-- Messages et erreurs
+	vim.cmd("highlight Error guifg=" .. red .. " gui=bold")
+	vim.cmd("highlight ErrorMsg guifg=" .. red .. " gui=bold")
+	vim.cmd("highlight WarningMsg guifg=" .. orange .. " gui=bold")
+	vim.cmd("highlight Todo guifg=#000000 guibg=" .. yellow .. " gui=bold")
 
-	-- Cursor line number
-	vim.cmd("highlight CursorLineNr guifg=#ffffff")
+	-- Interface utilisateur
+	vim.cmd("highlight Pmenu guibg=" .. selection)
+	vim.cmd("highlight PmenuSel guibg=#6c71c4 guifg=" .. fg)
+	vim.cmd("highlight LineNr guifg=" .. comment)
+	vim.cmd("highlight NonText guifg=" .. comment)
+	vim.cmd("highlight VertSplit guifg=" .. comment)
+	vim.cmd("highlight MatchParen guibg=NONE guifg=" .. pink .. " gui=underline,bold")
 
-	-- Status line
-	vim.cmd("highlight StatusLine guibg=#21252b guifg=#dcdcdc")
-
-	-- Match parenthesis
-	vim.cmd("highlight MatchParen guibg=#3e4451 guifg=#ffffff gui=bold")
-
-	-- Punctuation
-	vim.cmd("highlight Punctuation guifg=#dcdcdc")
-
-	-- Operators
-	vim.cmd("highlight Operator guifg=#dcdcdc")
-
-	-- Todo comments
-	vim.cmd("highlight Todo guifg=#ffcb6b guibg=#2b2d30 gui=bold")
-
-	-- Cursor
-	vim.cmd("highlight Cursor guifg=#2b2d30 guibg=#82aaff")
-
-	-- Tabs
-	vim.cmd("highlight TabLine guibg=#2b2d30 guifg=#4b5263")
-	vim.cmd("highlight TabLineSel guibg=#3e4451 guifg=#ffffff")
-	vim.cmd("highlight TabLineFill guibg=#2b2d30")
-
-	-- Popup menus
-	vim.cmd("highlight Pmenu guibg=#3e4451 guifg=#dcdcdc")
-	vim.cmd("highlight PmenuSel guibg=#4b5263 guifg=#ffffff")
-
-	-- Search highlight
-	vim.cmd("highlight Search guibg=#ffc107 guifg=#2b2d30")
-
-	-- Diff colors
-	vim.cmd("highlight DiffAdd guibg=#294436 guifg=#a3be8c")
-	vim.cmd("highlight DiffChange guibg=#3e4451 guifg=#e0af68")
-	vim.cmd("highlight DiffDelete guibg=#4b5263 guifg=#ff5370")
-	vim.cmd("highlight DiffText guibg=#3c4048 guifg=#82aaff")
+	-- Diff
+	vim.cmd("highlight DiffAdd guifg=" .. green .. " gui=bold")
+	vim.cmd("highlight DiffChange guifg=" .. orange .. " gui=bold")
+	vim.cmd("highlight DiffDelete guifg=" .. red .. " gui=bold")
+	vim.cmd("highlight DiffText guifg=" .. cyan .. " gui=bold")
 end
 
--- Apply the custom colorscheme
-custom_colorscheme()
+-- Appliquer le thème
+dracula_no_bg()
+
+-- Ajouter cette ligne à votre init.lua pour utiliser ce thème :
+-- require('dracula_no_bg')
